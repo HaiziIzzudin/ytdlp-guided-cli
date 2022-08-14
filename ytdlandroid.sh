8a32e6b38de6d9f075570a96174283bacfbf8b6a
HaiziIzzudin <79714350+HaiziIzzudin@users.noreply.github.com>
2022-08-15 04:31:27 +0800

    my stupid softwareid file has been removed, the package has its own updater. Tomorrow we do quality selection.

--- ytdlandroid.sh
+++ ytdlandroid.sh
@@ -10,7 +10,7 @@ magenta="\e[35m"
 cyan="\e[36m"
 e="\e[0m"
 
-changeDirToDownload () { # since termux by default did not open in user downloads
+ChangeDirToDownload () { # since termux by default did not open in user downloads
     cd ..
     cd ..
     cd ..
@@ -20,46 +20,31 @@ changeDirToDownload () { # since termux by default did not open in user download
     cd download
 }
 
-CheckPrereq () { # check if yt-dlp and ffmpeg available in device (provided by github.com/lostb053/ytdl-termux)
-
-    if [ -f "/sdcard/download/ytdlhaizi-softwareid" ]; then
-        echo "Prerequisites file still exists. Proceeding to next step..."
-    else 
-        echo -e "${yellow}You don't have prerequisites available. Allow software to download it first...${e}"
-        echo
-        DownloadAndInstallPrereq
-        changeDirToDownload
-        touch ytdlhaizi-softwareid
-        echo
-        echo -e "${yellow}A software identifier has been made in your Downloads directory.${e}"
-        echo -e "${red}DO NOT DELETE THIS FILE.${e}"
-        echo -e "${yellow}This file works as an identifier if prerequisites has been installed in your device.${e}"
-        echo -e "${yellow}Erasing this file can cause redundancy of installer and can take your precious storage space. Understood?${e}"
-        echo
-        read -p "Press enter to agree..."
-    fi
+InstallUpdatePrereq () { # install/update prereq packages (provided by github.com/lostb053/ytdl-termux)
+    bash -c "$(curl -fsSL https://bit.ly/install-ytdl-termux)"
+    # package has update script, no warning of overwrite will be issued.
 }
 
-DownloadAndInstallPrereq () {
-    bash -c "$(curl -fsSL https://bit.ly/install-ytdl-termux)"
+Header () {
+    echo -e "${green}      ======================================${e}"
+    echo -e "${green}       YOUTUBE DOWNLOADER BY HAIZI IZZUDIN${e}"
+    echo -e "${green}       ANDROID VERSION 0.2a${e}"
+    echo -e "${green}       POWERED BY YT-DLP AND FFMPEG${e}"
+    echo -e "${green}       INSTALL SCRIPT BY lostb053 ON GITHUB${e}"
+    echo -e "${green}      ======================================${e}"
+    echo
+}
+
+TypeNQualitySelection () {
+    echo -e "What kind of downloads do you want?"
 }
 
 # actual working program starts here...
-clear;
-echo -e "${green}      ======================================${e}"
-echo -e "${green}       YOUTUBE DOWNLOADER BY HAIZI IZZUDIN${e}"
-echo -e "${green}       ANDROID VERSION 0.2a${e}"
-echo -e "${green}       POWERED BY YT-DLP AND FFMPEG${e}"
-echo -e "${green}       INSTALL SCRIPT BY lostb053 ON GITHUB${e}"
-echo -e "${green}      ======================================${e}"
-echo 
-echo -e "${green}This program will download the best quality available to your Downloads folder.${e}"
-echo 
-echo -e "${yellow}Paste YouTube URL:${e}"
-read youtubelink
+clear; Header;
+read -e -p "${yellow}Paste YouTube URL: ${e}" youtubelink
 echo
-CheckPrereq # perform file checks b4 downloading
-changeDirToDownload # cd to downloads folder to download video to working dir
+InstallUpdatePrereq # perform prereq checks b4 downloading
+ChangeDirToDownload # cd to downloads folder to download video on
 echo -e "${yellow}Starting download...${e}"
 echo
 yt-dlp $youtubelink -f "bv+ba" -o "%(playlist_autonumber)d %(channel)s %(id)s.%(ext)s"
