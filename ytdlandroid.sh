621155b55bec074147d28c1a795ddfdd607aa163
HaiziIzzudin <79714350+HaiziIzzudin@users.noreply.github.com>
2022-08-15 00:23:56 +0800

    Added CheckPrereq

--- ytdlandroid.sh
+++ ytdlandroid.sh
@@ -20,14 +20,37 @@ changeDirToDownload () { # since termux by default did not open in user download
     cd download
 }
 
+CheckPrereq () { # check if yt-dlp and ffmpeg available in device (provided by github.com/lostb053/ytdl-termux)
+
+    if [ -f "/sdcard/ytdlhaizi-softwareid" ]; then
+        echo "Prerequisites file still exists. Proceeding to next step..."
+    else 
+        echo -e "${yellow}You don't have prerequisites available. Allow software to download it first...${e}"
+        DownloadAndInstallPrereq
+        touch ytdlhaizi-softwareid
+        echo "${yellow}A software identifier has been made in your directory.${e}"
+        echo "${red}DO NOT DELETE THIS FILE.${e}"
+        echo "${yellow}This file works as an identifier if prerequisites has been installed in your device.${e}"
+        echo "${yellow}Erasing this file can cause redundancy of installer and can take your precious storage space. Understood?${e}"
+        read -p "Press enter to agree."
+    fi
+}
+
+DownloadAndInstallPrereq () {
+    bash -c "$(curl -fsSL https://bit.ly/install-ytdl-termux)"
+}
+
+
+
 # actual working program starts here...
-echo -e "${green}YOUTUBE DOWNLOADER BY HAIZI IZZUDIN: ANDROID VERSION 0.1a${e}"
+echo -e "${green}YOUTUBE DOWNLOADER BY HAIZI IZZUDIN: ANDROID VERSION 0.2a${e}"
 echo -e "${green}POWERED BY YT-DLP AND FFMPEG, MODULE INSTALLS BY github.com/lostb053${e}"
 echo 
 echo -e "${green}This program will download the best quality available to the /Downloads directory.${e}"
 echo 
 echo -e "${yellow}Enter YouTube URL:${e}"
 read youtubelink
+CheckPrereq # perform file checks b4 downloading
 changeDirToDownload # cd to downloads folder to download video to working dir
 echo 
 yt-dlp $youtubelink -f "bv+ba"
