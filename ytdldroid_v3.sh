ada6b1c88fef185070e8c195fbc2266e2cd3e0a7
HaiziIzzudin <79714350+HaiziIzzudin@users.noreply.github.com>
2022-08-15 07:25:35 +0800

    qewqwe

--- ytdldroid_v3.sh
+++ ytdldroid_v3.sh
@@ -1,9 +1,9 @@
-red="\e[31m"
-green="\e[32m"
-yellow="\e[33m"
+r="\e[31m"
+g="\e[32m"
+y="\e[33m"
 e="\e[0m"
 
-DownloadsDoneMessage () { echo -e "${green}Downloads done. It should be in your Downloads folder.${e}" }
+DownloadsDoneMessage () { echo -e "${g}Downloads done. It should be in your Downloads folder.${e}" }
 
 BeforeDownloadRoutine () {
     bash -c "$(curl -fsSL https://bit.ly/install-ytdl-termux)";
@@ -17,77 +17,83 @@ BeforeDownloadRoutine () {
 }
 
 TypeNQualitySelectionSingleMedia () {
-    echo -e "${yellow}What kind of downloads do you want?${e}"
-    echo -e "${yellow}[A] Audio Only${e}"
-    echo -e "${yellow}[V] YouTube Video${e}"
-    echo -e "${yellow}[N] Non-YouTube Video${e}"
-    echo -e "${yellow}Enter neither will exit the program.${e}"
+    echo -e "${y}What kind of downloads do you want?${e}"
+    echo -e "${y}[A] Audio Only${e}"
+    echo -e "${y}[V] YouTube Video${e}"
+    echo -e "${y}[N] Non-YouTube Video${e}"
+    echo -e "${y}Enter neither will exit the program.${e}"
     read -e -p "Pick Your Poison: " typeselection
 	
-    if [ "$typeselection" = "A" ]; then
-        echo "${yellow}Audio only download selected. Downloading...${e}"
-        BeforeDownloadRoutine;
+    if [ "$typeselection" = "A" ]
+    then
+        echo "${y}Audio only download selected. Downloading...${e}"
+        BeforeDownloadRoutine
         yt-dlp $youtubelink -f "ba" -o "%(title)s.%(ext)s"
-        DownloadsDoneMessage;
+        DownloadsDoneMessage
 		
-    elif [ "$typeselection" = "N" ]; then
-        echo "${yellow}Non-YouTube Video download selected. Downloading...${e}"
-        echo "${yellow}(By default, this will download in highest quality, due to most non video centric website usually stream 720p max)${e}"
-        BeforeDownloadRoutine;
+    elif [ "$typeselection" = "N" ]
+    then
+        echo "${y}Non-YouTube Video download selected. Downloading...${e}"
+        echo "${y}(By default, this will download in highest quality, due to most non video centric website usually stream 720p max)${e}"
+        BeforeDownloadRoutine
         yt-dlp $youtubelink -f "bv" -o "%(title)s.%(ext)s"
-        DownloadsDoneMessage;
+        DownloadsDoneMessage
 		
-    elif [ "$typeselection" = "V" ]; then
-        echo "${yellow}YouTube Video download selected. Choose resolution...${e}"
-        echo "${yellow}[1080] 1920x1080 video${e}"
-        echo "${yellow}[2K] 2560x1440 video${e}"
-        echo "${yellow}[4K] 3840x2160 video${e}"
-        echo "${yellow}(entering neither, or non-availability of resolution selected, will result to program selecting the highest resolution video available)${e}"
+    elif [ "$typeselection" = "V" ]
+    then
+        echo "${y}YouTube Video download selected. Choose resolution...${e}"
+        echo "${y}[1080] 1920x1080 video${e}"
+        echo "${y}[2K] 2560x1440 video${e}"
+        echo "${y}[4K] 3840x2160 video${e}"
+        echo "${y}(entering neither, or non-availability of resolution selected, will result to program selecting the highest resolution video available)${e}"
         read -e -p "Pick a resolution: " resolution
 		
-        if [ "$resolution" = "1080" ]; then
-            echo "${yellow}1080p resolution selected. Downloading...${e}"
-            BeforeDownloadRoutine;
+        if [ "$resolution" = "1080" ]
+        then
+            echo "${y}1080p resolution selected. Downloading...${e}"
+            BeforeDownloadRoutine
             yt-dlp $youtubelink -f "bv*[width<=1920]+ba" -o "%(title)s.%(ext)s"
-            DownloadsDoneMessage;
+            DownloadsDoneMessage
 			
-        elif [ "$resolution" = "2K" ]; then
-            echo "${yellow}2K resolution selected. Downloading...${e}"
-            BeforeDownloadRoutine;
+        elif [ "$resolution" = "2K" ]
+        then
+            echo "${y}2K resolution selected. Downloading...${e}"
+            BeforeDownloadRoutine
             yt-dlp $youtubelink -f "bv*[width<=2560]+ba" -o "%(title)s.%(ext)s"
-            DownloadsDoneMessage;
+            DownloadsDoneMessage
 			
-        elif [ "$resolution" = "4K" ]; then
-            echo "${yellow}4K resolution selected. Downloading...${e}"
-            BeforeDownloadRoutine;
+        elif [ "$resolution" = "4K" ]
+        then
+            echo "${y}4K resolution selected. Downloading...${e}"
+            BeforeDownloadRoutine
             yt-dlp $youtubelink -f "bv*[width<=3840]+ba" -o "%(title)s.%(ext)s"
-            DownloadsDoneMessage;
+            DownloadsDoneMessage
 			
         else
-            echo "${yellow}Neither of resolution entered matched. Downloading highest quality video available...${e}"
-            BeforeDownloadRoutine;
+            echo "${y}Neither of resolution entered matched. Downloading highest quality video available...${e}"
+            BeforeDownloadRoutine
             yt-dlp $youtubelink -f "bv*+ba" -o "%(title)s.%(ext)s"
-            DownloadsDoneMessage;
+            DownloadsDoneMessage
         fi
     else
-        echo "${red}Neither of options entered matched. Exiting...${e}"
-        exit;
+        echo "${r}Neither of options entered matched. Exiting...${e}"
+        exit
     fi
 }
 
 EnterYTLink () { read -e -p "Paste YouTube URL: " youtubelink }
 
 Header () {
-    echo -e "${green}      ======================================${e}"
-    echo -e "${green}       YOUTUBE DOWNLOADER BY HAIZI IZZUDIN${e}"
-    echo -e "${green}       ANDROID VERSION 0.2a${e}"
-    echo -e "${green}       POWERED BY YT-DLP AND FFMPEG${e}"
-    echo -e "${green}       INSTALL SCRIPT BY lostb053 ON GITHUB${e}"
-    echo -e "${green}      ======================================${e}"
+    echo -e "${g}      ======================================${e}"
+    echo -e "${g}       YOUTUBE DOWNLOADER BY HAIZI IZZUDIN${e}"
+    echo -e "${g}       ANDROID VERSION 0.2a${e}"
+    echo -e "${g}       POWERED BY YT-DLP AND FFMPEG${e}"
+    echo -e "${g}       INSTALL SCRIPT BY lostb053 ON GITHUB${e}"
+    echo -e "${g}      ======================================${e}"
     echo
 }
 
-clear; 
-Header; 
-EnterYTLink; 
-TypeNQualitySelectionSingleMedia;
+clear
+Header
+EnterYTLink
+TypeNQualitySelectionSingleMedia
