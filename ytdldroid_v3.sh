3c0377ae22570f0dd927f21757015103d2e7f3f8
HaiziIzzudin <79714350+HaiziIzzudin@users.noreply.github.com>
2022-08-15 08:01:19 +0800

    qweweqwe

--- ytdldroid_v3.sh
+++ ytdldroid_v3.sh
@@ -16,57 +16,57 @@ TypeNQualitySelectionSingleMedia () {
 	
     if [ "$typeselection" = "A" ]
     then
-        echo "${y}Audio only download selected. Downloading...${e}"
+        echo -e "${y}Audio only download selected. Downloading...${e}"
         BeforeDownloadRoutine
         yt-dlp $youtubelink -f "ba" -o "%(title)s.%(ext)s"
         DownloadsDoneMessage
 		
     elif [ "$typeselection" = "N" ]
     then
-        echo "${y}Non-YouTube Video download selected. Downloading...${e}"
-        echo "${y}(By default, this will download in highest quality, due to most non video centric website usually stream 720p max)${e}"
+        echo -e "${y}Non-YouTube Video download selected. Downloading...${e}"
+        echo -e "${y}(By default, this will download in highest quality, due to most non video centric website usually stream 720p max)${e}"
         BeforeDownloadRoutine
         yt-dlp $youtubelink -f "bv" -o "%(title)s.%(ext)s"
         DownloadsDoneMessage
 		
     elif [ "$typeselection" = "V" ]
     then
-        echo "${y}YouTube Video download selected. Choose resolution...${e}"
-        echo "${y}(1080) 1920x1080 video${e}"
-        echo "${y}(2K) 2560x1440 video${e}"
-        echo "${y}(4K) 3840x2160 video${e}"
-        echo "${y}(entering neither, or non-availability of resolution selected, will result to program selecting the highest resolution video available)${e}"
+        echo -e "${y}YouTube Video download selected. Choose resolution...${e}"
+        echo -e "${y}(1080) 1920x1080 video${e}"
+        echo -e "${y}(2K) 2560x1440 video${e}"
+        echo -e "${y}(4K) 3840x2160 video${e}"
+        echo -e "${y}(entering neither, or non-availability of resolution selected, will result to program selecting the highest resolution video available)${e}"
         read -e -p "Pick a resolution: " resolution
 		
         if [ "$resolution" = "1080" ]
         then
-            echo "${y}1080p resolution selected. Downloading...${e}"
+            echo -e "${y}1080p resolution selected. Downloading...${e}"
             BeforeDownloadRoutine
             yt-dlp $youtubelink -f "bv*[width<=1920]+ba" -o "%(title)s.%(ext)s"
             DownloadsDoneMessage
 			
         elif [ "$resolution" = "2K" ]
         then
-            echo "${y}2K resolution selected. Downloading...${e}"
+            echo -e "${y}2K resolution selected. Downloading...${e}"
             BeforeDownloadRoutine
             yt-dlp $youtubelink -f "bv*[width<=2560]+ba" -o "%(title)s.%(ext)s"
             DownloadsDoneMessage
 			
         elif [ "$resolution" = "4K" ]
         then
-            echo "${y}4K resolution selected. Downloading...${e}"
+            echo -e "${y}4K resolution selected. Downloading...${e}"
             BeforeDownloadRoutine
             yt-dlp $youtubelink -f "bv*[width<=3840]+ba" -o "%(title)s.%(ext)s"
             DownloadsDoneMessage
 			
         else
-            echo "${y}Neither of resolution entered matched. Downloading highest quality video available...${e}"
+            echo -e "${y}Neither of resolution entered matched. Downloading highest quality video available...${e}"
             BeforeDownloadRoutine
             yt-dlp $youtubelink -f "bv*+ba" -o "%(title)s.%(ext)s"
             DownloadsDoneMessage
         fi
     else
-        echo "${r}Neither of options entered matched. Exiting...${e}"
+        echo -e "${r}Neither of options entered matched. Exiting...${e}"
         exit
     fi
 }
