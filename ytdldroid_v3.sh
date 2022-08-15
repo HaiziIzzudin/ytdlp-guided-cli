555150f64be10d4e5b523a918faddb8d96b989bf
HaiziIzzudin <79714350+HaiziIzzudin@users.noreply.github.com>
2022-08-15 18:34:42 +0800

    download more

--- ytdldroid_v3.sh
+++ ytdldroid_v3.sh
@@ -1,6 +1,18 @@
 r="\e[31m"; g="\e[32m"; y="\e[33m"; e="\e[0m"; # color presets
 
-DownloadsDoneMessage () { echo -e "${g}Downloads done. It should be in your Downloads folder.${e}"; }
+DownloadsDoneMessage () { 
+    echo -e "${g}Downloads done. It should be in your Downloads folder.${e}"
+    echo
+    echo "Key in C to download more. Key in other to exit program."
+    read -e -p "Option: " continueorabort
+
+    if [[ "$continueorabort" =~ (C|c) ]]
+    then # back to input yt url
+        MainMenu
+     
+     else # exit program
+        exit;
+}
 
 BeforeDownloadRoutine () {
     bash -c "$(curl -fsSL https://bit.ly/install-ytdl-termux)"; cd ..; cd ..; cd ..; cd ..; cd ..; cd sdcard; cd download;
@@ -34,40 +46,37 @@ TypeNQualitySelectionSingleMedia () {
         echo "VP9 has lower compression quality in a cost of a lower cpu usage. Recommended because its a common codec widely adopted by media players."
         echo -e "${y}(2P9) 2560x1440 VP9 video${e}"
         echo -e "${y}(4P9) 3840x2160 VP9 video${e}"
+        echo
         read -e -p "Pick a resolution: " resolution
 		
         if [[ "$resolution" =~ (2V1|2v1) ]]
         then # Download 2k resolution av1 video
             BeforeDownloadRoutine
             yt-dlp $youtubelink -S "res:1440,vcodec:av1"
-            DownloadsDoneMessage
-			
+        	
         elif [[ "$resolution" =~ (4V1|4v1) ]]
         then # Download 4k resolution av1 video
             BeforeDownloadRoutine
             yt-dlp $youtubelink -S "res:2160,vcodec:av1"
-            DownloadsDoneMessage
-			
+        	
         
         elif [[ "$resolution" =~ (2P9|2p9) ]]
         then # Download 4k resolution vp9 video
             BeforeDownloadRoutine
             yt-dlp $youtubelink -S "res:1440,vcodec:vp9"
-            DownloadsDoneMessage
-
+        
         elif [[ "$resolution" =~ (4p9|4P9) ]]
         then # Download 4k resolution vp9 video
             BeforeDownloadRoutine
             yt-dlp $youtubelink -S "res:2160,vcodec:vp9"
-            DownloadsDoneMessage
-
+        
         
         else # Download 1080p resolution h264 video
             BeforeDownloadRoutine
             yt-dlp $youtubelink -S "res:1080,vcodec:h264"
-            DownloadsDoneMessage
         fi
     fi
+    DownloadsDoneMessage
 }
 
 MainMenu () {
