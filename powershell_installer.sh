a655881c5e06b016acf220e7317232282116b30f
CupcakKe fan <79714350+HaiziIzzudin@users.noreply.github.com>
2022-09-10 02:16:34 +0800

    Update powershell_installer.sh

--- powershell_installer.sh
+++ powershell_installer.sh
@@ -1,17 +1,1 @@
-# Install system components
-sudo apt update && sudo apt install -y curl gnupg apt-transport-https;
-
-# Import the public repository GPG keys
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -;
-
-# Register the Microsoft Product feed
-sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-debian-bullseye-prod bullseye main" > /etc/apt/sources.list.d/microsoft.list';
-
-# Install PowerShell
-sudo apt update && sudo apt install -y powershell;
-
-# Start PowerShell
-pwsh;
-
-# start yt-dlp
-iwr rebrand.ly/ytdlpgcli | iex;
+sudo apt update && sudo apt install -y curl gnupg apt-transport-https;curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -;sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-debian-bullseye-prod bullseye main" > /etc/apt/sources.list.d/microsoft.list';sudo apt update && sudo apt install -y powershell;
