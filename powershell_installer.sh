d1e07bf70bd280d3c05255f15ea77cf260e0512c
CupcakKe fan <79714350+HaiziIzzudin@users.noreply.github.com>
2022-09-10 02:31:28 +0800

    Delete powershell_installer.sh

--- powershell_installer.sh
+++
@@ -1,1 +0,0 @@
-sudo apt update && sudo apt install -y curl gnupg apt-transport-https;curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -;sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-debian-bullseye-prod bullseye main" > /etc/apt/sources.list.d/microsoft.list';sudo apt update && sudo apt install -y powershell;
