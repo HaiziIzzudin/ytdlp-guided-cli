e90a978676b723c7ae6635417b03a6fd04669eec
Nama diberi Haizi <79714350+HaiziIzzudin@users.noreply.github.com>
2022-12-20 09:39:48 +0800

    1st test

---
+++ livedl_forAndroid_v4.sh
@@ -0,0 +1,26 @@
+r="\e[31m"; g="\e[32m"; y="\e[33m"; e="\e[0m"; # color presets
+
+
+echo "Updating ytdlp for android...";
+
+
+bash -c "$(curl -fsSL https://bit.ly/install-ytdl-termux)";
+cd ..; cd ..; cd ..; cd ..; cd ..; cd sdcard; cd download;
+
+
+# user input livedl
+read -e -p "Enter in live URL: " yturl
+read -e -p "Enter in game name: " gamename
+
+
+# naming scheme
+d=`date +%Y%m%d`; foldername="$d_ $gamename";
+
+
+# Check if file gameName already available. If not, create one. Then, cd into it.
+if [[ -d "$foldername" ]]
+then
+    cd $foldername;
+else
+    mkdir $foldername; cd $foldername;
+fi
